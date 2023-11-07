eb3ad41df2c0b641fee3668eeffce44b9311025e
Ethan Farmer <efarmer743@outlook.com>
2023-11-07 13:11:34 -0500

    Adds walls and a ceiling. Also track the thuds on those objects.

--- Farmer_CGT215_Lab07.cpp
+++ Farmer_CGT215_Lab07.cpp
@@ -9,25 +9,66 @@ using namespace sfp;
 using namespace sf;
 
 int main() {
-    // Create our window and world with gravity 0,1
+    // Create our window and world with gravity 0,1.
     RenderWindow window(VideoMode(800, 600), "Bounce");
     World world(Vector2f(0, 1));
 
-    // Create the ball
+    // Create the ball.
     PhysicsCircle ball;
     ball.setCenter(Vector2f(400, 300));
+    ball.applyImpulse(Vector2f(0.2, 0.1));
     ball.setRadius(20);
     world.AddPhysicsBody(ball);
 
-    // Create the floor
+    // Create the floor.
     PhysicsRectangle floor;
     floor.setSize(Vector2f(800, 20));
     floor.setCenter(Vector2f(400, 590));
     floor.setStatic(true);
     world.AddPhysicsBody(floor);
 
+    // Create the ceiling. 
+    PhysicsRectangle ceiling;
+    ceiling.setSize(Vector2f(800, 20));
+    ceiling.setCenter(Vector2f(400, 10));
+    ceiling.setStatic(true);
+    world.AddPhysicsBody(ceiling);
+
+    // Create the left wall.
+    PhysicsRectangle leftWall;
+    leftWall.setSize(Vector2f(20, 600));
+    leftWall.setCenter(Vector2f(790, 300));
+    leftWall.setStatic(true);
+    world.AddPhysicsBody(leftWall);
+
+    // Create the right wall.
+    PhysicsRectangle rightWall;
+    rightWall.setSize(Vector2f(20, 600));
+    rightWall.setCenter(Vector2f(10, 300));
+    rightWall.setStatic(true);
+    world.AddPhysicsBody(rightWall);
+
+    // Counts the "thuds" when floor collision happens.
     int thudCount(0);
     floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+        cout << "thud " << thudCount << endl;
+        thudCount++;
+    };
+
+    // Counts the "thuds" when ceiling collision happens.
+    ceiling.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+        cout << "thud " << thudCount << endl;
+        thudCount++;
+    };
+
+    // Counts the "thuds" when left wall collision happens.
+    leftWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+        cout << "thud " << thudCount << endl;
+        thudCount++;
+    };
+
+    // Counts the "thuds" when right wall collision happens.
+    rightWall.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
         cout << "thud " << thudCount << endl;
         thudCount++;
     };
@@ -35,7 +76,7 @@ int main() {
     Clock clock;
     Time lastTime(clock.getElapsedTime());
     while (true) {
-        // calculate MS since last frame
+        // Calculate MS since last frame.
         Time currentTime(clock.getElapsedTime());
         Time deltaTime(currentTime - lastTime);
         int deltaTimeMS(deltaTime.asMilliseconds());
@@ -46,6 +87,9 @@ int main() {
         window.clear(Color(0, 0, 0));
         window.draw(ball);
         window.draw(floor);
+        window.draw(ceiling);
+        window.draw(leftWall);
+        window.draw(rightWall);
         window.display();
     }
 }
